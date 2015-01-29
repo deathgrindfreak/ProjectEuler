3ca6f91b455a4aa423c7dc4ccba5ab3696bb2e75
Cooper Bell <john.cooper.bell@gmail.com>
2015-01-29 17:10:56 -0600

    added prob60

--- ProjectEuler.hs
+++ ProjectEuler.hs
@@ -1,8 +1,7 @@
 module ProjectEuler (sieveOfEratosthenes) where
 
 sieveOfEratosthenes :: Int -> [Int]
-sieveOfEratosthenes n = 2 : sieve intLst where
-  intLst = [3, 5..n]
+sieveOfEratosthenes n = 2 : sieve [3, 5..n] where
   sieve [] = []
   sieve (x:xs) = x : sieve (filter (\y -> mod y x /= 0) xs)
   
