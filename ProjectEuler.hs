ec396ec96bf3d97e640dcf59ccf9bedbcb811ca1
Cooper Bell <john.cooper.bell@gmail.com>
2015-04-24 07:40:13 -0500

    Added problem 81

--- ProjectEuler.hs
+++ ProjectEuler.hs
@@ -5,3 +5,25 @@ sieveOfEratosthenes n = 2 : sieve [3, 5..n] where
   sieve [] = []
   sieve (x:xs) = x : sieve (filter (\y -> mod y x /= 0) xs)
   
+
+-- millerRabin :: Int -> Int -> Bool
+-- millerRabin n k
+--   | n < 3     = n == 2
+--   | otherwise = witnessLoop 0
+--   where witnessLoop = undefined
+        -- factorN m = factorLoop 0 (m - 1)
+        -- factorLoop x y 
+        --   | mod y 2 == 0 = factorLoop (x + 1) (y / 2)
+        --   | otherwise    = (x, y)
+        
+millerRabin :: Int -> Int -> Bool
+millerRabin n k
+  | n < 3     = n == 2
+  | otherwise = let factorN m = factorLoop 0 (m - 1)
+                    factorLoop x y 
+                      | mod y 2 == 0 = factorLoop (x + 1) (y / 2)
+                      | otherwise    = (x, y)
+                in witnessLoop 0
+  where witnessLoop w = let (s, d) = factorN n
+                      in if w < k then
+                           let a = undefined
--- prob81.hs
+++ prob81.hs
@@ -1,14 +1,54 @@
 module Main (main) where
 
 import System.IO
+import Data.List
 import Data.List.Split
 
 
+-- Find the minimum sum path
+minPath :: [[Int]] -> Int
+minPath matrix =
+  let l = length matrix
+      left = reverse $ take (quot (l - 1) 2) matrix
+      center = matrix !! quot (l - 1) 2
+      right = drop (quot (l + 1) 2) matrix
+  in iterPath left center right
+
+iterPath [] [c] [] = c
+iterPath (l:ls) c (r:rs) = iterPath ls (addMins l c r) rs
+
+addMins _ [c] _ = []
+addMins (l:ls) (fc:sc:c) (r:rs)
+  | fc < sc   = (l + fc + r) : addMins ls (sc:c) rs
+  | otherwise = (l + sc + r) : addMins ls (sc:c) rs
+
+
+-- Produces a matrix of diagonals as rows
+rotateMatrix rms [] = rms
+rotateMatrix rms (m:ms) =
+  let zp = zipRow m rms in
+   (head zp) : rotateMatrix (tail zp) ms
+
+
+-- Zips a list onto a list of lists
+zipRow :: [Int] -> [[Int]] -> [[Int]]
+zipRow [] ys = ys
+zipRow (x:xs) [] = [x] : zipRow xs []
+zipRow (x:xs) (y:ys)= (x : y) : zipRow xs ys
+
+
+-- Parse the matrix into a proper array
 parseMatrix :: String -> [[Int]]
 parseMatrix = map (map (\num -> read num :: Int) . splitOn ",") . lines
 
-main = do
-  withFile "p081_matrix.txt" ReadMode
-    (\handle -> do
-        contents <- hGetContents handle
-        print $ parseMatrix contents)
+
+-- Get the result
+getResult :: String -> Int
+getResult = minPath . rotateMatrix [] . parseMatrix
+
+
+main :: IO ()
+main = withFile "p081_matrix.txt" ReadMode
+       (\handle -> do
+           contents <- hGetContents handle
+           print $ getResult contents)
